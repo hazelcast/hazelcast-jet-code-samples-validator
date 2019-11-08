5049c8e20be249e230206403e221d63840850ff4
Ondrej Lukas <ondrej@hazelcast.com>
2019-11-08 12:55:40 +0100

    Added first bulk of scripts

---
+++ event-journal/event-journal-CacheJournalSource.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export SCRIPT_WORKSPACE=$1
+export JET_REPO=$2
+
+export OUTPUT_LOG_FILE=${SCRIPT_WORKSPACE}/output.log
+
+function check_text_in_log {
+    EXPECTED_TEXT=$1
+    echo "Checking log for '${EXPECTED_TEXT}'"
+    EXPECTED_TEXT_COUNT=$(grep "${EXPECTED_TEXT}" ${OUTPUT_LOG_FILE} | wc -l)
+    if [ ${EXPECTED_TEXT_COUNT} -lt 1 ]; then   
+        echo "Log '${EXPECTED_TEXT}' has not been found in output log.";
+        exit 1
+    fi
+}
+
+cd ${JET_REPO}
+mvn clean install -U -B -Dmaven.test.failure.ignore=true -DskipTests
+
+###########################
+### execute code sample ###
+###########################
+cd ${JET_REPO}/examples/event-journal
+mvn "-Dexec.args=-classpath %classpath com.hazelcast.jet.examples.eventjournal.CacheJournalSource" -Dexec.executable=java org.codehaus.mojo:exec-maven-plugin:1.6.0:exec | tee ${OUTPUT_LOG_FILE}
+
+#################################
+### verify code sample output ###
+#################################
+check_text_in_log "Read 1000 entries from cache journal."

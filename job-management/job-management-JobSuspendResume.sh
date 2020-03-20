acfc4cb8b5c4b5564ffc8e850b5121e4e9be755c
Ondrej Lukas <ondrej@hazelcast.com>
2020-03-20 14:11:37 +0100

    Add test for part of job-management

---
+++ job-management/job-management-JobSuspendResume.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+export SCRIPT_WORKSPACE=$1
+export JET_REPO=$2
+
+export OUTPUT_LOG_FILE=${SCRIPT_WORKSPACE}/output.log
+
+function check_text_in_log {
+    EXPECTED_TEXT=$1
+    EXPECTED_AT_LEAST=${2:-1}
+    echo "Checking log for '${EXPECTED_TEXT}'"
+    EXPECTED_TEXT_COUNT=$(grep "${EXPECTED_TEXT}" ${OUTPUT_LOG_FILE} | wc -l)
+    if [ ${EXPECTED_TEXT_COUNT} -lt ${EXPECTED_AT_LEAST} ]; then   
+        echo "Log '${EXPECTED_TEXT}' has not been found in output log.";
+        exit 1
+    fi
+}
+
+cd ${JET_REPO}
+mvn clean install -U -B -Dmaven.test.failure.ignore=true -DskipTests
+
+###########################
+### execute code sample ###
+###########################
+cd ${JET_REPO}/examples/job-management
+mvn "-Dexec.args=-classpath %classpath com.hazelcast.jet.examples.jobmanagement.JobSuspendResume" -Dexec.executable=java org.codehaus.mojo:exec-maven-plugin:1.6.0:exec | tee ${OUTPUT_LOG_FILE}
+
+#################################
+### verify code sample output ###
+#################################
+check_text_in_log "Job '.*' is submitted."
+check_text_in_log "Suspending the job..."
+check_text_in_log "Resuming the job..."
+check_text_in_log "Cancelling the job..."
+check_text_in_log "Job is cancelled."
+check_text_in_log "Status: FAILED"
+check_text_in_log "Job is RUNNING." 2
+check_text_in_log "Job is SUSPENDED."
+

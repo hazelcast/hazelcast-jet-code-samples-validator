9af4f225792ab1a7eb8eb191f2dd4399eee60574
Ondrej Lukas <ondrej@hazelcast.com>
2022-08-16 16:20:10 +0200

    Update zookeeper message check in kafka tests

--- kafka/kafka-KafkaJsonSource.sh
+++ kafka/kafka-KafkaJsonSource.sh
@@ -50,7 +50,7 @@ if [ ${EXPECTED_JOB_FINISH_LOG_COUNT} -lt 1 ]; then
     exit 1
 fi
 
-check_text_in_log "\[ZooKeeperClient\] Connected"
+check_text_in_log "\[ZooKeeperClient Kafka server\] Connected"
 check_text_in_log "\[KafkaServer id=0\] started"
 check_text_in_log "Received 20 entries in [0-9]* milliseconds."
 
